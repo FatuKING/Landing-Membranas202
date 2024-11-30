16c4851720940e62eff811d7c86b7124f4435b5f
Facundo Cozzani <cozzanifacundo@gmail.com>
2024-11-30 15:52:42 -0300

    menu burger in mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,16 @@ import { useEffect, useState } from "react";
 
 export function Navbar() {
   const [activeItem, setActiveItem] = useState("Inicio")
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleToggle = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
 
   const handleMenuClick = (item) => {
     setActiveItem(item)
-  };
+  }
 
   const controls = useAnimation();
 
@@ -18,12 +24,12 @@ export function Navbar() {
 
       if (scrollY > 0) {
         controls.start({
-          height: "60px",
+          height: "90px",
           transition: { duration: 0.3 },
         });
       } else {
         controls.start({
-          height: "90px",
+          height: "100px",
           transition: { duration: 0.3 },
         });
       }
@@ -36,36 +42,38 @@ export function Navbar() {
   return (
     <motion.header
       className="flex justify-center fixed top-0 z-10 w-full text-black bg-gray-850 border-b-2 border-b-orange-600"
-      initial={{ height: "90px"}}
+      initial={{ height: "100px"}}
       animate={controls}
     >
-      <nav className="flex justify-between items-center w-11/12 2xl:w-3/4 p-2 text-xl text-gray-100">
-        <div className="flex justify-center items-center">
-          <figure className="w-10 h-10">
-            <img className="w-full h-full" src={logoMembranas202} alt="Logo" />
-          </figure>
-          <h2 className="text-2xl text-oswald font-semibold">
-            Membranas <span className="">202</span>
-          </h2>
-        </div>
+      <article className="flex justify-between items-center w-11/12 2xl:w-3/4 p-2 text-xl text-gray-100">
+        <nav className={`${isMenuOpen ? 'flex flex-col' : 'flex justify-between w-full'}`}>
+          <div className="flex items-center">
+            <figure className="w-10 h-10">
+              <img className="w-full h-full" src={logoMembranas202} alt="Logo" />
+            </figure>
+            <h2 className="text-2xl text-oswald font-semibold">
+              Membranas <span className="">202</span>
+            </h2>
+          </div>
 
-        <NavbarToggle />
+          <ul className={`${isMenuOpen ? 'flex pt-1' : 'hidden'} text-base sm:text-lg lg:text-xl lg:flex gap-2 lg:gap-4`}>
+            <li className={`font-semibold itemNav ${activeItem === 'Inicio' ? "active" : ""}`}>
+              <a data-text="Inicio" href="#" onClick={() => handleMenuClick('Inicio')}>Inicio</a>
+            </li>
+            <li className={`font-semibold itemNav ${activeItem === 'Nosotros' ? "active" : ""}`}>
+              <a data-text="Nosotros" href="#nosotros" onClick={() => handleMenuClick('Nosotros')}>Nosotros</a>
+            </li>
+            <li className={`font-semibold itemNav ${activeItem === 'Servicios' ? "active" : ""}`}>
+              <a data-text="Servicios" href="#servicios" onClick={() => handleMenuClick('Servicios')}>Servicios</a>
+            </li>
+            <li className={`font-semibold itemNav ${activeItem === 'Contacto' ? "active" : ""}`}>
+              <a data-text="Contacto" href="#contacto" onClick={() => handleMenuClick('Contacto')}>Contacto</a>
+            </li>
+          </ul>
+        </nav>
 
-        <ul className="hidden lg:flex gap-4">
-          <li className={`font-semibold itemNav ${activeItem === 'Inicio' ? "active" : ""}`}>
-            <a data-text="Inicio" href="#" onClick={() => handleMenuClick('Inicio')}>Inicio</a>
-          </li>
-          <li className={`font-semibold itemNav ${activeItem === 'Nosotros' ? "active" : ""}`}>
-            <a data-text="Nosotros" href="#nosotros" onClick={() => handleMenuClick('Nosotros')}>Nosotros</a>
-          </li>
-          <li className={`font-semibold itemNav ${activeItem === 'Servicios' ? "active" : ""}`}>
-            <a data-text="Servicios" href="#servicios" onClick={() => handleMenuClick('Servicios')}>Servicios</a>
-          </li>
-          <li className={`font-semibold itemNav ${activeItem === 'Contacto' ? "active" : ""}`}>
-            <a data-text="Contacto" href="#contacto" onClick={() => handleMenuClick('Contacto')}>Contacto</a>
-          </li>
-        </ul>
-      </nav>
+        <NavbarToggle toggle={handleToggle}/>
+      </article>
     </motion.header>
   );
 }
--- src/components/NavbarToggle.jsx
+++ src/components/NavbarToggle.jsx
@@ -1,17 +1,8 @@
-import { useState } from "react"
-import { MenuMobile } from "./Portals/MenuMobile"
-
-export function NavbarToggle () {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-  const handleToggle = () => {
-    setIsMenuOpen(!isMenuOpen)
-  }
-
+export function NavbarToggle ({ toggle }) {
   return (
     <>
-      <label className='flex flex-col gap-2 w-8 lg:hidden'>
-        <input className='peer hidden' type='checkbox' onChange={handleToggle}/>
+      <label className='flex flex-col gap-2 w-8 lg:hidden' onChange={toggle}>
+        <input className='peer hidden' type='checkbox'/>
         <div
           className='rounded-2xl h-[3px] w-1/2 bg-orange-600  duration-500 peer-checked:rotate-[225deg] origin-right peer-checked:-translate-x-[12px] peer-checked:-translate-y-[1px]'
         />
@@ -22,8 +13,6 @@ export function NavbarToggle () {
           className='rounded-2xl h-[3px] w-1/2 bg-orange-600   duration-500 place-self-end peer-checked:rotate-[225deg] origin-left peer-checked:translate-x-[12px] peer-checked:translate-y-[1px]'
         />
       </label>
-
-      {isMenuOpen ? <MenuMobile/> : null}
     </>
   )
 }
