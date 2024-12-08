f109cde065e7529a162cc3feca3ad97045dcfa17
Facundo Cozzani <cozzanifacundo@gmail.com>
2024-12-08 19:42:40 -0300

    prueba3

--- src/components/SliderBar.jsx
+++ src/components/SliderBar.jsx
@@ -23,7 +23,7 @@ export function SliderBar() {
                     <div className="flex gap-4 mt-4 text-base sm:text-lg lg:text-xl">
                         <a 
                         target='_blank' 
-                        href="https://wa.me/1140963523?text=%C2%A1Hola!%20%F0%9F%91%8B%20Estoy%20interesado/a%20en%20la%20venta%20y%20colocaci%C3%B3n%20de%20membranas%20para%20techos.%20%F0%9F%8F%A0%20%C2%BFPodr%C3%ADan%20brindarme%20informaci%C3%B3n%20sobre%20precios%20y%20disponibilidad%3F%20Gracias." 
+                        href="https://wa.me/+5491140963523?text=%C2%A1Hola!%20%F0%9F%91%8B%20Estoy%20interesado/a%20en%20la%20venta%20y%20colocaci%C3%B3n%20de%20membranas%20para%20techos.%20%F0%9F%8F%A0%20%C2%BFPodr%C3%ADan%20brindarme%20informaci%C3%B3n%20sobre%20precios%20y%20disponibilidad%3F%20Gracias." 
                         className="bg-orange-500 text-black p-2 px-4 rounded hover:bg-orange-600">Contáctanos</a>
                         <a 
                         href="#nosotros" 
